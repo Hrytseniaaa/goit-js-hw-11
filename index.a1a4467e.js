0c81edbbf812f6e81cf22f656bcbb41631346a11
Hrytseniaaa <106759647+Hrytseniaaa@users.noreply.github.com>
2022-08-17 07:56:29 +0000

    Deploying to gh-pages from @ Hrytseniaaa/goit-js-hw-11@c99fba8d2b4f9ca645c031aa5d3ec2911fc941f9 🚀

---
+++ index.a1a4467e.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".search-form");console.log(e);e.addEventListener("submit",(e=>{e.preventDefault(),console.log("123")}));
+//# sourceMappingURL=index.a1a4467e.js.map
---
+++ index.cba970e0.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".search-form");console.log(e);e.addEventListener("submit",(function(e){e.preventDefault(),console.log("123")}))}();
+//# sourceMappingURL=index.cba970e0.js.map
